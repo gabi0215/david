46545eedc090335f3fb8cf34da23af9a2d089974
gabi0215 <tmdrkq0215@gmail.com>
2025-08-06 17:41:30 +0900

    Update app.py
    문제 요구사항 맞추기위한 커밋

--- common_solid_01/app.py
+++ common_solid_01/app.py
@@ -19,5 +19,9 @@ def menu():
 def test1():
     return render_template('test1.html')
 
+@app.route("/test2")
+def test1():
+    return render_template('test1.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
